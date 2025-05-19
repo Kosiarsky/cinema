b049656f3f6985401680fe7646f496a222395b96
Jakub Osełka <95690893+Kosiarsky@users.noreply.github.com>
2025-05-19 18:41:16 +0200

    Add login component

---
+++ frontend/src/app/login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ CommonModule, HeaderComponent, FooterComponent ],
+  templateUrl: './login.component.html',
+  styles: ``
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      console.log('Form Data:', this.loginForm.value);
+      // Dodaj logikę logowania
+    }
+  }
+
+  onRegister(): void {
+    console.log('Przejdź do rejestracji');
+    // Dodaj logikę przejścia do rejestracji
+  }
+}
