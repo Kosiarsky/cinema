91edfdec027fac92bbd4cd41bd30e7675617a81e
Jakub Osełka <95690893+Kosiarsky@users.noreply.github.com>
2025-05-19 18:41:38 +0200

    Add recommended component

---
+++ frontend/src/app/recommended/recommended.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recommended',
+  standalone: true,
+  imports: [],
+  templateUrl: './recommended.component.html',
+  styles: ``
+})
+export class RecommendedComponent {
+
+}
