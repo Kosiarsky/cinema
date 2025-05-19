65236edcde87e1728314c17ff6f70c3a22a668e5
Jakub Osełka <95690893+Kosiarsky@users.noreply.github.com>
2025-05-19 18:40:16 +0200

    App config

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, ReactiveFormsModule, HttpClientModule],
   template: `
       <router-outlet></router-outlet>
   `,
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,18 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component'; 
 import { RepertoireComponent } from './repertoire/repertoire.component';
+import { AnnouncementsComponent } from './announcements/announcements.component';
+import { NewsComponent } from './news/news.component';
+import { RecommendedComponent } from './recommended/recommended.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'repertoire', component: RepertoireComponent },
+    { path: 'announcements', component: AnnouncementsComponent },
+    { path: 'news', component: NewsComponent },
+    { path: 'recommended', component: RecommendedComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
 ];
