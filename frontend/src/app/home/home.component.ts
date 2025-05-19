2f9ef50a2fd0b9412daf412c476fa46d6ae9643a
Jakub Osełka <95690893+Kosiarsky@users.noreply.github.com>
2025-05-19 18:41:08 +0200

    Update home component

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -13,22 +13,64 @@ import { SliderComponent } from '../slider/slider.component';
 })
 export class HomeComponent {
   recommendedMovies = [
-    { title: 'Recommended Movie 1', description: 'Description of Recommended Movie 1' },
-    { title: 'Recommended Movie 2', description: 'Description of Recommended Movie 2' },
-    { title: 'Recommended Movie 3', description: 'Description of Recommended Movie 3' },
-    { title: 'Recommended Movie 4', description: 'Description of Recommended Movie 4' },
+    {
+      image: 'https://fwcdn.pl/fpo/15/75/10031575/8171655.8.webp',
+      title: 'Amator',
+      description: 'Poruszający dramat o pasji i wytrwałości w dążeniu do celu.'
+    },
+    {
+      image: 'https://fwcdn.pl/fpo/94/80/10009480/8166856.8.webp',
+      title: 'Oszukać przeznaczenie: Wieże krwi',
+      description: 'Film o walce z przeznaczeniem i tajemniczymi wydarzeniami.'
+    },
+    {
+      image: 'https://fwcdn.pl/fpo/88/48/10068848/8173706.8.webp',
+      title: 'Hurry Up Tomorrow',
+      description: 'Historia pełna emocji i niespodziewanych zwrotów akcji.'
+    },
+    {
+      image: 'https://fwcdn.pl/fpo/15/75/10031575/8171655.8.webp',
+      title: 'Amator',
+      description: 'Poruszający dramat o pasji i wytrwałości w dążeniu do celu.'
+    },
+    {
+      image: 'https://fwcdn.pl/fpo/88/48/10068848/8173706.8.webp',
+      title: 'Hurry Up Tomorrow',
+      description: 'Historia pełna emocji i niespodziewanych zwrotów akcji.'
+    }
   ];
 
   newsList = [
-    { title: 'News 1', summary: 'Summary of News 1' },
+    { title: 'Oszukać przeznaczenie: Więzy krwi nadchodzi!', summary: 'Najnowsza odsłona krwawego i kasowego cyklu studia New Line Cinema, zabiera widzów do samych początków tego, jak śmierć zaczęła realizować swoje obłąkane poczucie sprawiedliwości. Prześladowana strasznym, powtarzającym się koszmarem studentka Stefanie wraca do rodzinnego miasta. Chce odnaleźć jedyną osobę, która może przerwać błędne koło i ocalić członków jej rodziny przed niechybnie czekającą ich makabryczną śmiercią…' },
     { title: 'News 2', summary: 'Summary of News 2' },
     { title: 'News 3', summary: 'Summary of News 3' },
   ];
 
   upcomingMovies = [
-    { title: 'Upcoming Movie 1', description: 'Description of Upcoming Movie 1' },
-    { title: 'Upcoming Movie 2', description: 'Description of Upcoming Movie 2' },
-    { title: 'Upcoming Movie 3', description: 'Description of Upcoming Movie 3' },
-    { title: 'Upcoming Movie 4', description: 'Description of Upcoming Movie 4' },
+    {
+      image: 'https://fwcdn.pl/fpo/15/75/10031575/8171655.8.webp',
+      title: 'Amator',
+      description: 'Poruszający dramat o pasji i wytrwałości w dążeniu do celu.'
+    },
+    {
+      image: 'https://fwcdn.pl/fpo/94/80/10009480/8166856.8.webp',
+      title: 'Oszukać przeznaczenie: Wieże krwi',
+      description: 'Film o walce z przeznaczeniem i tajemniczymi wydarzeniami.'
+    },
+    {
+      image: 'https://fwcdn.pl/fpo/88/48/10068848/8173706.8.webp',
+      title: 'Hurry Up Tomorrow',
+      description: 'Historia pełna emocji i niespodziewanych zwrotów akcji.'
+    },
+    {
+      image: 'https://fwcdn.pl/fpo/15/75/10031575/8171655.8.webp',
+      title: 'Amator',
+      description: 'Poruszający dramat o pasji i wytrwałości w dążeniu do celu.'
+    },
+    {
+      image: 'https://fwcdn.pl/fpo/88/48/10068848/8173706.8.webp',
+      title: 'Hurry Up Tomorrow',
+      description: 'Historia pełna emocji i niespodziewanych zwrotów akcji.'
+    }
   ];
 }
