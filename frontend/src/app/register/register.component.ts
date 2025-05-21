758037f5bf72ab6d866e5a10b4a8ad5bf84ad475
Jakub Osełka <95690893+Kosiarsky@users.noreply.github.com>
2025-05-21 10:42:04 +0200

    Update register page

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ CommonModule, RouterLink ],
+  imports: [CommonModule, RouterLink, HeaderComponent, FooterComponent],
   templateUrl: './register.component.html',
-  styles: ``
+  styles: ``,
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required, Validators.pattern(/^\d{9,15}$/)]], // Walidacja numeru telefonu
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required]],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.registerForm.valid) {
+      console.log('Form Data:', this.registerForm.value);
+      // Dodaj logikę rejestracji
+    }
+  }
 }
